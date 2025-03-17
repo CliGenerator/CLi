31fd74ae88e17243caccecb6442cbbef2fa2f3dc
aadarsh214 <aadarshgupta7828@gmail.com>
2025-03-17 16:54:15 +0530

    Add main application component with routing and context providers

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Index from "./pages/Index";
+import Resources from "./pages/Resources";
+import Roadmaps from "./pages/Roadmaps";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import Learn from "./pages/Learn";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <AuthProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/roadmaps" element={<Roadmaps />} />
+            <Route path="/learn" element={<Learn />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/:section" element={<Profile />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
